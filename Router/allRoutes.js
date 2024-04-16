84f2e92d3c20ec446759583e600e030d19e27427
Baha-Abdelatif <baha.abdelatif@gmail.com>
2024-04-16 22:56:29 +0200

    Mise en place du système de routage, et des pages de base du site.

---
+++ Router/allRoutes.js
@@ -0,0 +1,10 @@
+import Route from "./Route.js";
+
+//Définir ici vos routes
+export const allRoutes = [
+  new Route("/", "Accueil", "/pages/home.html"),
+  new Route("/Galerie", "Galerie", "/pages/galerie.html", "/js/galerie.js"),
+];
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Quai Antique";
