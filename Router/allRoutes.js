dfbef3b8076dc84922d081ce27b37b1496474b4e
Baha-Abdelatif <baha.abdelatif@gmail.com>
2024-04-17 16:15:27 +0200

    Styles du Header

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,7 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
-  new Route("/Galerie", "Galerie", "/pages/galerie.html", "/js/galerie.js"),
+  new Route("/galerie", "Galerie", "/pages/galerie.html", "/js/galerie.js"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
