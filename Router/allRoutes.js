48ba552e52d5dfb047d1e16ebd06739edcc338de
Baha-Abdelatif <baha.abdelatif@gmail.com>
2024-04-17 16:21:17 +0200

    Ajout des pages correspondant à la navigation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,15 @@ import Route from "./Route.js";
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/galerie", "Galerie", "/pages/galerie.html", "/js/galerie.js"),
+  new Route("/carte", "Carte", "/pages/carte.html", "/js/carte.js"),
+  new Route(
+    "/reservations",
+    "Reservations",
+    "/pages/reservations.html",
+    "/js/reservations.js"
+  ),
+  new Route("/compte", "Compte", "/pages/compte.html", "/js/compte.js"),
+  new Route("/connect", "Connect", "/pages/connect.html", "/js/connect.js"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
